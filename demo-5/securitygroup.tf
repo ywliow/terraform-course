38f633cb38c9e9e12ff85a11c4d1764166991b26
ywliow <ywliow@gmail.com>
2022-03-25 08:35:19 +0800

    Completed Course

--- demo-5/securitygroup.tf
+++ demo-5/securitygroup.tf
@@ -1,20 +1,21 @@
-data "aws_ip_ranges" "european_ec2" {
-  regions  = ["eu-west-1", "eu-central-1"]
+data "aws_ip_ranges" "southeast_ec2" {
+  regions  = ["ap-southeast-1"]
   services = ["ec2"]
 }
 
-resource "aws_security_group" "from_europe" {
-  name = "from_europe"
+resource "aws_security_group" "ap_southeast-1" {
+  name        = "ap_southeast-1"
+  description = "ap-southeast-1 IP addressses range"
 
   ingress {
     from_port   = "443"
     to_port     = "443"
     protocol    = "tcp"
-    cidr_blocks = slice(data.aws_ip_ranges.european_ec2.cidr_blocks, 0, 50)
+    cidr_blocks = slice(data.aws_ip_ranges.southeast_ec2.cidr_blocks, 0, 40)
   }
   tags = {
-    CreateDate = data.aws_ip_ranges.european_ec2.create_date
-    SyncToken  = data.aws_ip_ranges.european_ec2.sync_token
+    CreateDate = data.aws_ip_ranges.southeast_ec2.create_date
+    SyncToken  = data.aws_ip_ranges.southeast_ec2.sync_token
   }
 }
 
--- demo-5/vars.tf
+++ demo-5/vars.tf
@@ -1,13 +1,14 @@
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "ap-southeast-1"
 }
 
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-844e0bf7"
+    us-east-1      = "ami-13be557e"
+    us-west-2      = "ami-06b94666"
+    eu-west-1      = "ami-844e0bf7"
+    ap-southeast-1 = "ami-0f74c08b8b5effa56"
   }
 }
 
